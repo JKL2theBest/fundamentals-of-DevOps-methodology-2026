c4f9fdd0e7e4cdf170246a0bc47b3875bbba25ad
JKL2theBest <muhammet.jkl2.suhanguylev@gmail.com>
2026-04-12 14:37:47 +0300

    lab3 kinda done

---
+++ lab3/app2/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return f"<h1>САЙТ 2 - Второй пет проект</h1><p>Запрос пришел с IP: {request.headers.get('X-Real-IP', request.remote_addr)}</p>"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
